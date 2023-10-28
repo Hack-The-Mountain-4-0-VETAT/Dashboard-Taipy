0f3174f926c99c5f453d4445bbc12e621732dfac
Om Tiwari <83060926+Om-Tiwari@users.noreply.github.com>
2023-10-28 23:45:16 +0530

    final update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from taipy.gui import Gui
+import taipy as tp
 import pandas as pd
-
+from gevent.pywsgi import WSGIServer
 section_1 = """
 ## Power Consumption Dashboard
 <|{dataset}|chart|x=YearMonth|y[1]=Usage|color=blue|>
@@ -11,8 +12,8 @@ dataset = pd.read_csv("data.csv")
 gui = Gui(page=section_1)
 
 if __name__ == '__main__':
-    gui.run(title='Dashboard',
-    		dark_mode=False)
-else:
     app = gui.run(title='Dashboard',
-                  dark_mode=False)
+    		dark_mode=False)
+    
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
